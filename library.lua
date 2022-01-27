d0e23f552a87c57fe21b7e6cae4c95576e087414
vibing_guy <61268411+vibing-guy@users.noreply.github.com>
2022-01-28 00:08:43 +0800

    Update library.lua

--- library.lua
+++ library.lua
@@ -1609,10 +1609,16 @@ local UnlockMouse
 function library:Init()
 	
 	self.base = self.base or self:Create("ScreenGui")
-	if syn and syn.protect_gui then
+	if (syn and syn.protect_gui) then
 		syn.protect_gui(self.base)
-	else get_hidden_gui then
-		get_hidden_gui(self.base)
+		self.base.Parent = game:GetService("CoreGui")
+	elseif type(get_hidden_gui) == 'function' then
+		self.base.Parent = get_hidden_gui()
+	elseif type(gethui) == 'function' then
+		self.base.Parent = gethui()
+	else
+		self.base.Name = tostring(math.random())
+		self.base.Parent = game:GetService("CoreGui")
 	end
 	self.base.Parent = game:GetService"CoreGui"
 	
